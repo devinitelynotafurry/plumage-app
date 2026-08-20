ff723f5a2bfd61615bfc1ebafe98b9a0b6bdaf4f
devinitelynotafurry <devinitelynotafurry@users.noreply.github.com>
2026-08-20 13:23:03 -0500

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         // WRITE_EXTERNAL_STORAGE path back in MediaExporter.
         minSdk = 29
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+	versionCode = 2
+	versionName = "1.0.1"
         vectorDrawables.useSupportLibrary = true
     }
     buildTypes {
